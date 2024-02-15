ef37f644d49d4560e7618e5c4355ea845af42ecf
ThomazFB <thomazfcortez@gmail.com>
2024-02-14 22:33:55 -0300

    Define PasskeyRequestTest file

---
+++ WordPressLoginFlow/src/test/java/org/wordpress/android/login/webauthn/PasskeyRequestTest.kt
@@ -0,0 +1,26 @@
+package org.wordpress.android.login.webauthn
+
+import org.junit.Test
+
+class PasskeyRequestTest {
+    @Test
+    fun `when passkey request is created, then use the request data to create a get credential request`() {
+        // Given
+        // When
+        // Then
+    }
+
+    @Test
+    fun `when passkey request succeeds, then create the payload as expected`() {
+        // Given
+        // When
+        // Then
+    }
+
+    @Test
+    fun `when passkey request fails, then call the onFailure as expected`() {
+        // Given
+        // When
+        // Then
+    }
+}
