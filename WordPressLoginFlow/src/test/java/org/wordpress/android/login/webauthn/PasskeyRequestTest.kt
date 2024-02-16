782bf111fb0f105f50194c7abdbc6a7aaffba21c
ThomazFB <thomazfcortez@gmail.com>
2024-02-16 15:47:44 -0300

    Add basic mock and coroutines setup for PasskeyRequestTest

--- WordPressLoginFlow/src/test/java/org/wordpress/android/login/webauthn/PasskeyRequestTest.kt
+++ WordPressLoginFlow/src/test/java/org/wordpress/android/login/webauthn/PasskeyRequestTest.kt
@@ -1,8 +1,23 @@
 package org.wordpress.android.login.webauthn
 
+import android.content.Context
+import android.os.CancellationSignal
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito.mock
+import java.util.concurrent.ExecutorService
 
+
+@OptIn(ExperimentalCoroutinesApi::class)
 class PasskeyRequestTest {
+    lateinit var context: Context
+    lateinit var executor: ExecutorService
+    lateinit var signal: CancellationSignal
+    private val coroutineScope: CoroutineScope = CoroutineScope(UnconfinedTestDispatcher())
+
     @Test
     fun `when passkey request is created, then use the request data to create a get credential request`() {
         // Given
